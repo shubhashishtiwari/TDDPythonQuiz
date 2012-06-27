ec2bf8260bcd023ed0d830f080f2a623364e590e
shubhashishtiwari <shubhasheesh.tiwari@gmail.com>
2012-06-27 14:42:41 +0530

    Update master

--- TestFizzBuzzMocked.py
+++ TestFizzBuzzMocked.py
@@ -4,6 +4,18 @@ import FizzBuzz
 """
 Q5. Write the psuedocode for the test_repport method, such that it uses PyMock
     mock objects to test the report method of FizzBuzz. [5 pts]
+
+    def test_report(self):
+        mockFile = self.mock()
+        mockFileWrapper = self.mockFile('fizzbuzz_report.txt', 'w')
+        #replay
+        self.replay()
+        #Call the report method with mocked filewrapper
+        numbers=range(100)
+        FizzBuzz.report(self.numbers,self.mockFileWrapper)
+        #verify
+        self.verify()
+
 """
 class TestFizzBuzzMocked(pymock.PyMockTestCase):
         
