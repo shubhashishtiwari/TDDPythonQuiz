f52d255956344aae3ff0790d421f34cec810df11
shubhashishtiwari <shubhasheesh.tiwari@gmail.com>
2012-06-27 14:33:30 +0530

    Answered the questions

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,11 +2,54 @@
 Q1. Why is the report method untestable ? [2 pts]
 
 
+1)open command is an external collaborator, which should be passed as an argument to the report method
+2)File handler to be created for the write and close method
 
 
 Q2. How will you change the api of the report method to make it more testable ? [2 pts]
 
+The defintion of report will accept three arguments report_file(self, numbers,fileWrapper)
+the calls to open , write and close will be substituted by the fileWrapper.
+This wrapper can be mocked and code logic can be tested accordingly.
 
+
+class FizzBuzz(object):
+def report(self, numbers,filehandle):
+
+filehandle.open()
+for number in numbers:
+msg = str(number) + " "
+fizzbuzz_found = False
+if number % 3 == 0:
+msg += "fizz "
+fizzbuzz_found = True
+if number % 5 == 0:
+msg += "buzz "
+fizzbuzz_numbersfound = True
+
+if fizzbuzz_found:
+filehandle.write(msg + "\n")
+
+filehandle.close()
+
+if "__main__" == __name__:
+fb = FizzBuzz()
+wrapper = filewrapper('fizzbuzz_report.txt', 'w')
+fb.report(range(100),wrapper)
+
+class filewrapper:
+def __init__(self,fname,mode):
+self.fname = fname
+self.mode = mode
+def open(self):
+self.fh = open(self.fname,self.mode)
+return self.fh
+
+def write(self,msg):
+self.fh.write(msg)
+
+def close(self):
+self.fh.close()
 
 """
 class FizzBuzz(object):
