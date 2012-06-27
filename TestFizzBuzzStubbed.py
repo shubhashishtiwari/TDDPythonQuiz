8a27f3d6f17d916ca32ca98931c6a8e80b35c8a2
shubhashishtiwari <shubhasheesh.tiwari@gmail.com>
2012-06-27 14:38:42 +0530

    Implemented mystub class for the external contributors

--- TestFizzBuzzStubbed.py
+++ TestFizzBuzzStubbed.py
@@ -4,10 +4,15 @@ import FizzBuzz
 """
 Q3. What will be printed when we execute 'python FizzBuzzStubbed.py' ? [3 pts]
 
-
-
-
-
+setUpClass FizzBuzzStubbed
+setup
+test_report
+teardown
+test_report
+setup
+test_report
+teardown
+tearDownClass
 
 
 
@@ -15,6 +20,16 @@ Q3. What will be printed when we execute 'python FizzBuzzStubbed.py' ? [3 pts]
 Q4. Implement MyStub class so that you can send it as a fake object to the
     report method of FizzBuzz object from a test case. [3 pts]
 
+class MyStub(object):
+    def __init__(self):
+        self.openflag=False
+        self.values=[] 
+    def open(self):
+        self.openflag = True 
+    def write(self,msg):
+        self.values.append(msg) 
+    def close():
+        self.openflag = False # close fake "file"
 """
 class MyStub(object):
     pass
